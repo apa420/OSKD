37ec71d94b8ae9efb2662499ac645521ed7eeb58
apa420 <apa420@apa420.com>
2025-02-08 21:29:57 +0100

    wayland implementation

--- public/app.js
+++ public/app.js
@@ -3,146 +3,161 @@ const container = document.getElementById("keys");
 
 // Config provider.
 let config = {
-  options: {
-    doNotRepeatLastKey: false,
-    printOnlyCombinations: false,
-    rowLayout: "",
-  },
-  modifiersKeys: [],
-  keyMapping: {},
-  keyStyle: {},
+    options: {
+        rowLayout: "",
+    },
+    modifierKeys: [],
+    keyMapping: {},
+    keyStyle: {},
 };
 
 function setConfig(newConfig) {
-  config = newConfig;
-  container.classList.add(config.options.rowLayout);
+    config = newConfig;
+    container.classList.add(config.options.rowLayout);
 }
 
 fetch("/config.json")
-  .then((res) => res.json())
-  .then(setConfig);
+    .then((res) => res.json())
+    .then(setConfig);
 
 // Mapping and utils.
 const COMMONS =
-  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890".split("");
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890".split("");
 
-function fixKey({ name, code }) {
-  const mapped = config.keyMapping[code];
-  if (mapped) {
-    return mapped;
-  } else if (name) {
-    return name;
-  } else if (code.startsWith("Key")) {
-    return code.split("Key")[1];
-  } else {
-    return code;
-  }
-}
-
-function isCommon(key) {
-  return COMMONS.includes(fixKey(key));
-}
-
-function isModifierKey(code) {
-  return config.modifiersKeys.includes(code);
-}
-
-// UI Handler.
-const combination = {
-  keys: [],
-  element: null,
-  update: null,
-};
-const keys = [];
-
-function addKey(key) {
-  const keyElement = document.createElement("div");
-  keyElement.classList.add("key");
-  applyConfigStyling(keyElement);
-  keyElement.innerText = key;
-  keys.push(keyElement);
-  container.appendChild(keyElement);
-
-  if (keys.length > 20) {
-    keys.shift().remove();
-  }
-
-  return keyElement;
+function applyStyleConfig(element, configKey, styleName = configKey) {
+    const cfg = config["keyStyle"][configKey];
+    if (cfg) {
+        element.style[styleName] = cfg;
+    }
 }
 
 function applyConfigStyling(element) {
-  applyStyleConfig(element, "fontSize");
-  applyStyleConfig(element, "textColor", "color");
-  applyStyleConfig(element, "backgroundColor");
-  applyStyleConfig(element, "borderRadius");
-  applyStyleConfig(element, "padding");
+    applyStyleConfig(element, "fontSize");
+    applyStyleConfig(element, "textColor", "color");
+    applyStyleConfig(element, "backgroundColor");
+    applyStyleConfig(element, "borderRadius");
+    applyStyleConfig(element, "padding");
 }
 
-function applyStyleConfig(element, configKey, styleName = configKey) {
-  const cfg = config["keyStyle"][configKey];
-  if (cfg) {
-    element.style[styleName] = cfg;
-  }
+const sortModifiers = (a, b) => {
+    if (config.modifierKeys[a] > config.modifierKeys[b]) {
+        return 1;
+    }
+    if (config.modifierKeys[a] < config.modifierKeys[b]) {
+        return -1;
+    }
+    return 0;
 }
 
-function addCombination() {
-  const element = addKey("");
-  combination.keys = [];
-  combination.element = element;
-  combination.update = () => {
-    element.innerText = combination.keys
-      .map((key) => fixKey({ code: key.code }))
-      .join(" + ");
-  };
+const data = {
+    lastKey: "",
+    modifiers: [],
+    modChanged: true,
+    repeat: 1,
+    prevElement: null,
+    prevCommon: false,
+    keys: []
+};
+
+function isCommon(keyName) {
+    return COMMONS.includes(keyName);
 }
 
-function onKeyPress(name, code) {
-  const key = { name, code };
 
-  if (combination.element) {
-    const last = combination.keys[combination.keys.length - 1];
-
-    if (config.options.doNotRepeatLastKey && last?.code == code) {
-      return;
+function mapKey(keyName, modifiers) {
+    let shiftPressed = false;
+    if (modifiers.includes("Shift_L") || modifiers.includes("Shift_R")) {
+        shiftPressed = true;
     }
 
-    combination.keys.push(key);
-    combination.update();
-  } else {
-    if (isModifierKey(code)) {
-      addCombination();
-      combination.keys.push(key);
-      combination.update();
-    } else if (config.options.printOnlyCombinations) {
-      addKey(fixKey(key));
+    let mapped = config.keyMapping[keyName];
+    if (mapped) {
+        return mapped;
     }
-  }
+
+    if (shiftPressed) {
+        let mappedShift = config.shiftKey[keyName];
+        if (mappedShift) {
+            return mappedShift;
+        }
+    } else if (keyName.length === 1) {
+        let charVal = keyName.charCodeAt(0);
+        if (64 < charVal && charVal < 91) {
+            console.log("keycode!!");
+            return String.fromCharCode(keyName.charCodeAt(0) + 32);
+        }
+    }
+    return keyName;
 }
 
-function onKeyRelease(code) {
-  if (combination.element) {
-    const first = combination.keys[0];
-    if (first.code == code) {
-      combination.element = null;
+function onKeyPress(keyName) {
+    if (config.modifierKeys.hasOwnProperty(keyName)) {
+        data.modChanged = true;
+        data.prevCommon = false;
+        data.modifiers.push(keyName);
+        return;
     }
-  }
+    let parsedKey = mapKey(keyName, data.modifiers);
+
+    if (data.modChanged === true || data.prevCommon !== isCommon(parsedKey)) {
+        const keyElement = document.createElement("div");
+        keyElement.classList.add("key");
+        applyConfigStyling(keyElement);
+
+        data.modifiers = data.modifiers.sort(sortModifiers);
+
+        for (const mod of data.modifiers) {
+            keyElement.innerText += mapKey(mod, data.modifiers) + " + ";
+        }
+        keyElement.innerText += parsedKey;
+
+        data.keys.push(keyElement);
+        container.appendChild(keyElement);
+        prevElement = keyElement;
+
+        if (data.keys.length > 20) {
+            data.keys.shift().remove();
+        }
+    } else {
+        prevElement.innerText += parsedKey;
+    }
+
+
+
+
+    if (prevElement.innerText.length > 20) {
+        data.modChanged = true;
+    } else {
+        data.modChanged = false;
+    }
+    data.prevCommon = isCommon(keyName);
+    data.lastKey = keyName;
+}
+
+function onKeyRelease(keyName) {
+    let index = data.modifiers.indexOf(keyName);
+    if (index !== -1) {
+        data.modifiers.splice(index, 1);
+        data.modChanged = true;
+    }
 }
 
 // Socket connection.
 const socket = io();
 
 socket.on("connect", () => {
-  console.log("Connected socket to backend.");
+    console.log("Connected socket to backend.");
 });
 
 socket.on("input", (packet) => {
-  const { name, event_type } = JSON.parse(packet);
-  const event = Object.keys(event_type)[0];
-  const data = event_type[event];
+    const split = packet.split(" ");
+    const key = split[0];
+    const press_type = split[1];
 
-  if (event == "KeyPress") {
-    onKeyPress(name, data);
-  } else if (event == "KeyRelease") {
-    onKeyRelease(data);
-  }
+    if (press_type == "1") {
+        onKeyPress(key);
+    } else if (press_type == "0") {
+        onKeyRelease(key);
+    }
 });
+
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,26 @@
-use axum::{body::Body, routing::get, Server};
-use http::Response;
-use rdev::{listen, Event};
+use axum::{response::Html, routing::get, Server};
+use input::{
+    event::{
+        keyboard::{KeyState, KeyboardEventTrait},
+        Event::Keyboard,
+        KeyboardEvent::Key,
+    },
+    Libinput, LibinputInterface,
+};
+use key_names::physical_key_name;
+use libc::{O_RDONLY, O_RDWR, O_WRONLY};
 use socketio_server::{
     config::SocketIoConfig, layer::SocketIoLayer, ns::Namespace, socket::Socket,
 };
+use std::fs::{File, OpenOptions};
+use std::os::unix::{fs::OpenOptionsExt, io::OwnedFd};
 use std::{
     env, fs,
+    path::Path,
     sync::{Arc, Mutex},
     time::Duration,
 };
+use winit::keyboard::{NativeKeyCode, PhysicalKey};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -35,12 +47,26 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .build();
 
     // Listen keyboard.
-    let callback = move |event: Event| {
-        let json = serde_json::to_string(&event).unwrap();
-        let clients_lock = clients.lock().unwrap();
-
-        for socket in clients_lock.iter() {
-            socket.emit("input", json.clone()).unwrap();
+    let callback = move |event: &mut input::Event| {
+        if let Keyboard(Key(eventk)) = event {
+            let name =
+                physical_key_name(PhysicalKey::Unidentified(NativeKeyCode::Xkb(eventk.key())));
+            match eventk.key_state() {
+                KeyState::Pressed => {
+                    let output = name + " 1";
+                    let clients_lock = clients.lock().unwrap();
+                    for socket in clients_lock.iter() {
+                        socket.emit("input", output.clone()).unwrap();
+                    }
+                }
+                KeyState::Released => {
+                    let output = name + " 0";
+                    let clients_lock = clients.lock().unwrap();
+                    for socket in clients_lock.iter() {
+                        socket.emit("input", output.clone()).unwrap();
+                    }
+                }
+            }
         }
     };
 
@@ -50,7 +76,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             "/",
             get(move || async {
                 let html = include_str!("../public/index.html");
-                Response::new(Body::from(html))
+                //Response::new(Body::from(html))
+                Html(html)
             }),
         )
         .route(
@@ -93,11 +120,30 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             .await
             .unwrap();
     });
+    struct Interface;
 
+    impl LibinputInterface for Interface {
+        fn open_restricted(&mut self, path: &Path, flags: i32) -> Result<OwnedFd, i32> {
+            OpenOptions::new()
+                .custom_flags(flags)
+                .read((flags & O_RDONLY != 0) | (flags & O_RDWR != 0))
+                .write((flags & O_WRONLY != 0) | (flags & O_RDWR != 0))
+                .open(path)
+                .map(|file| file.into())
+                .map_err(|err| err.raw_os_error().unwrap())
+        }
+        fn close_restricted(&mut self, fd: OwnedFd) {
+            drop(File::from(fd));
+        }
+    }
+
+    let mut input = Libinput::new_with_udev(Interface);
+    input.udev_assign_seat("seat0").unwrap();
     println!("Listening for keyboard input");
-    if let Err(error) = listen(callback) {
-        println!("Error listening for Keyboard input: {:?}", error)
-    };
-
-    Ok(())
+    loop {
+        input.dispatch().unwrap();
+        for mut event in &mut input {
+            callback(&mut event);
+        }
+    }
 }
